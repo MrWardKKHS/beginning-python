87629dd9e9fddf4c1a9b60b1cdd0db5aa7411a72
MrWardKKHS <60589645+MrWardKKHS@users.noreply.github.com>
2024-07-29 02:23:24 +0000

    Updated address

--- exercise-1/e1p1.py
+++ exercise-1/e1p1.py
@@ -6,5 +6,5 @@ https://www.youtube.com/watch?v=eVrM5WsMgrk&list=PL-NaYeCEwvWjZIC7aTKUvQwb-_1Sy8
 1.	Write a program that will print out your name, address and telephone number each on a separate line.
 """
 print("Hi!, my name is Alex!")
-print("123 Fakestreet")
+print("My new address is 84 I wont say Drive")
 print("1234567890")
