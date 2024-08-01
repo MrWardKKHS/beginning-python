7b2f837756c34aedf788f0600d642de8f156c1d3
MrWardKKHS <60589645+MrWardKKHS@users.noreply.github.com>
2024-08-01 03:19:38 +0000

    revert

--- exercise-1/e1p4.py
+++ exercise-1/e1p4.py
@@ -5,11 +5,3 @@ ______ inches is equal to _______ cm
 
 (1 inch = 2.54 cm)
 """
-inches = int(input("How many inches would you like to convert\n"))
-print(f"{inches} inches is {inches * 2.54}cm")
-
-
-
-
-8 
-"8"
