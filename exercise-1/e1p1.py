f4d495e64432d7160ecac9bdc0fafd536f736329
MrWardKKHS <60589645+MrWardKKHS@users.noreply.github.com>
2024-08-12 12:12:30 +1200

    Update e1p1.py

--- exercise-1/e1p1.py
+++ exercise-1/e1p1.py
@@ -8,3 +8,5 @@ https://www.youtube.com/watch?v=eVrM5WsMgrk&list=PL-NaYeCEwvWjZIC7aTKUvQwb-_1Sy8
 print("Hi!, my name is Alex!")
 print("My new address is 84 I wont say Drive")
 print("1234567890")
+
+# This work is fantastic!
