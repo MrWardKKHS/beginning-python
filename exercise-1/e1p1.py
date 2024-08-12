a50ef8f1f85f7759c8bc405933da3ecfcf78eaed
MrWardKKHS <60589645+MrWardKKHS@users.noreply.github.com>
2024-08-12 00:14:09 +0000

    made some changes

--- exercise-1/e1p1.py
+++ exercise-1/e1p1.py
@@ -8,7 +8,6 @@ https://www.youtube.com/watch?v=eVrM5WsMgrk&list=PL-NaYeCEwvWjZIC7aTKUvQwb-_1Sy8
 print("Hi!, my name is Alex!")
 print("My new address is 84 I wont say Drive")
 print("1234567890")
-
 # This work is fantastic!
 
 # this is also another change
